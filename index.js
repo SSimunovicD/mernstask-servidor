01cd404bdf98b97236dd8a22d96ed187f2f55295
Sebastián Simunovic <ssimunovic02@gmail.com>
2020-11-03 07:41:02 -0300

    ajustes para deployment

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({ extend: true }));
 
 // puerto de la app
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 
 // importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
