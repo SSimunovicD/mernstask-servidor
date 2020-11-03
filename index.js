d88ef3135c22d480c9d6db462c4abfe6d4912fc5
Sebastián Simunovic <ssimunovic02@gmail.com>
2020-11-03 07:29:12 -0300

    ajustes para deployment

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({ extend: true }));
 
 // puerto de la app
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 // importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -24,6 +24,6 @@ app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
 // arrancar la app
-app.listen(PORT, () => {
-    console.log(`El servidor está funcionando en el puerto ${PORT}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`El servidor está funcionando en el puerto ${port}`);
 });
